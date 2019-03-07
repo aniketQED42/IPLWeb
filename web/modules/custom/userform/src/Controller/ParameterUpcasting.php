2c7e893b596b9af5edf44bcd93cef6fbae532a39
aniketQED42 <aniket.gaikwad@QED42.com>
2019-03-07 10:55:34 +0530

    Added LoggerFactory using DI

--- web/modules/custom/userform/src/Controller/ParameterUpcasting.php
+++ web/modules/custom/userform/src/Controller/ParameterUpcasting.php
@@ -5,17 +5,20 @@ namespace Drupal\userform\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\Entity\Node;
 use Drupal\user\UserInterface;
+use Drupal\node\NodeInterface;
 
 class ParameterUpcasting extends ControllerBase {
 
     /**
      * Returns a render-able array for a test page.
      */
-    public function content($nodeid) {
-      $node = Node::load($nodeid);
+    public function content(NodeInterface $node) {
+    //   $node = Node::load($nodeid);
+    $author = $node->getOwner();
+    $author_name = $author->getUsername();
       $build = [
         '#type' => 'markup',
-        '#markup' => '<h1><b>' . t('Hello @aname!', array('@aname' => $node->getOwner()->getDisplayName())),
+        '#markup' => '<h1><b>' . t('Hello @aname!', array('@aname' => $author_name)),
       ]; 
       return $build;
     }
--- web/modules/custom/userform/src/Form/SimpleFormBase.php
+++ web/modules/custom/userform/src/Form/SimpleFormBase.php
@@ -3,8 +3,20 @@ namespace Drupal\userform\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class SimpleFormBase extends FormBase{
+    protected $loggerFactory;
+    /**
+     * Constructor.
+     */
+    public function __construct(LoggerChannelFactory $loggerFactory) {
+     $this->loggerFactory = $loggerFactory->get('userform');
+   }
+   public static function create(ContainerInterface $container){
+     return new static ($container->get('logger.factory'));
+   }
 
   public function getFormId() {
     return 'userform_set';
@@ -48,10 +60,10 @@ class SimpleFormBase extends FormBase{
       if(!preg_match("/^([a-zA-Z' ]+)$/",$name)){
         $form_state->setErrorByName('name',$this->t('Invaid Name!'));
       }
-    }
+    }  
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // foreach ($form_state->getValues() as $key => $values){
+        $this->loggerFactory->notice('Name : ' . $form_state->getValue('name') . '<br>Email : ' . $form_state->getValue('email') );
         $this->messenger()->addMessage('Name' . ': ' . $form_state->getValue('name'));
         $this->messenger()->addMessage('Email' . ': ' . $form_state->getValue('email'));
     // }
